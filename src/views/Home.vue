df187199bfae20c99f441db1eef32ea4e5bfbcf0
Adler Santos Rodrigues <adlerdet@outlook.com>
2020-08-24 19:18:45 -0300

    :art: Changing home style

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,14 @@
 <template>
   <div class="home">
+    <button @click="logout">Sair :(</button>
+    <form>
+      <h1>Add To-Do</h1>
+      <label for="title">Title:</label>
+      <input type="text" name="title" id="title" v-model="title">
+      <label for="content">Content:</label>
+      <input type="text" name="content" id="content" v-model="content">
+      <button v-on:click.prevent="handleNewTodo">Enviar :)</button>
+    </form>
     <ul v-for="todo in todos"  v-bind:key="todo.id">
       <li v-bind:v-if="todo.checked === false">
         <input type="checkbox" v-bind:checked="todo.checked" @click="handleCheckTodo(todo.id)">
@@ -19,12 +28,39 @@ import { Component, Vue } from 'vue-property-decorator';
 })
 
 export default class Home extends Vue {
-  todos: Array<any> = []
+  title: string = '';
+  content: string = '';
+  todos: Array<any> = [];
   $api: any;
 
   mounted() {
     this.handleTodos()
   }
+
+  public async handleNewTodo(): Promise<any>{
+    if(!this.title || !this.content){
+      console.error('Seu Serelepe, não deixe o input vazio');
+    } else {
+      try {
+        const token = localStorage.getItem('@todoToday:token');
+        const res = await this.$api.post('/todos',{
+            title: this.title, 
+            content: this.content
+          }, {
+          headers: {
+            'Authorization': `Bearer ${token}`
+          }
+        })
+
+        this.todos.push(res.data);
+        this.title = '';
+        this.content = '';
+      } catch (error) {
+        console.log(error)
+      }
+    }
+  }
+
 
   public async handleTodos(): Promise<any>{
     try {
@@ -39,6 +75,12 @@ export default class Home extends Vue {
     } catch (error) {
       console.log(error)
     }
+  }
+
+  public logout(): void{
+    localStorage.clear();
+
+    this.$router.push('/');
   }
 
   public async handleCheckTodo(id: string): Promise<any>{
@@ -74,5 +116,12 @@ export default class Home extends Vue {
 <style scoped>
   .home ul li{
     list-style: none;
+    border: solid 1px black;
+    width: 200px;
+    margin-top: 10px;
+  }
+
+  input {
+    border: solid 1px red;
   }
 </style>
